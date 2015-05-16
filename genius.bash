04e418b01737ec904b1775bc24402b0afc0c8806
gbell12 <greg@bitwombat.com.au>
2015-05-16 21:35:48 +1000

    cleanup

---
+++ genius.bash
@@ -0,0 +1,1 @@
+xsetwacom set "WALTOP International Corp. Graphics Tablet stylus" "Button" 2 3
---
+++ trackman.bash
@@ -0,0 +1,4 @@
+xmodmap -e "pointer = 1 2 3 4 5 6 7 8 9 10 11 12 13"
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation Button" 8 9
+xinput set-int-prop "Logitech USB Trackball" "Evdev Wheel Emulation" 8 1
+xinput set-int-prop "Logitech USB Trackball" "Evdev Middle Button Emulation" 8 1
