8a1fe06f860d5878e143d16c933697dd8252fc08
Greg Bell <greg@bitwombat.com.au>
2016-09-10 23:00:03 +1000

    Genericize

--- setup.sh
+++ setup.sh
@@ -3,9 +3,6 @@
 cd ~
 mv -f .bashrc dotbashrc_old
 ln -s .myconfigs/local/.bashrc
-ln -s .myconfigs/.inputrc
-ln -s .myconfigs/local/.vimrc
-ln -s .myconfigs/.xprofile
-ln -s .myconfigs/.Xmodmap
-ln -s .myconfigs/.Xkbmap
-ln -s .myconfigs/.conkyrc
+for i in .myconfigs/.*; do
+    ln -s $i
+done
